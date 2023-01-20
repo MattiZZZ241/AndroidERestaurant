582220ab3d4ed29db3360edbf2fe928675c5e66f
Matthis BERNET <matthis2401@gmail.com>
2023-01-20 16:54:59 +0100

    release + apk + jks

--- app/src/main/java/fr/isen/bernet/androiderestaurant/FoodDetailsActivity.kt
+++ app/src/main/java/fr/isen/bernet/androiderestaurant/FoodDetailsActivity.kt
@@ -3,10 +3,13 @@ package fr.isen.bernet.androiderestaurant
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.isen.bernet.androiderestaurant.databinding.ActivityFoodDetailsBinding
+import fr.isen.bernet.androidrestaurant.Items
 
 class FoodDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFoodDetailsBinding
+
+    private lateinit var dish: Items
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
